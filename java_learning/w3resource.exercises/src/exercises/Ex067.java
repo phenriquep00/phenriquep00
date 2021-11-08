1dac46e46f6f967ea7ad8e281d4be989a9a32844
phenriquep00 <phenriquep00@gmail.com>
2021-11-08 16:51:26 -0300

    updated exercises

--- java_learning/w3resource.exercises/src/exercises/Ex067.java
+++ java_learning/w3resource.exercises/src/exercises/Ex067.java
@@ -5,6 +5,19 @@ public class Ex067 {
     // Insert "Tutorial" in the middle of "Python 3.0", so result will be Python Tutorial 3.0
     // Python Tutorial 3.0
     public static void main(String[] args){
+        String myStr = "Python 3.0";
+        String[] arr = myStr.split(" ");
+        System.out.println(myStr);
+        myStr = "";
         
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 1) {
+                myStr += " Tutorial ";
+            } else {
+                myStr += arr[i];
+            }
+        }
+        myStr += "3.0";
+        System.out.println(myStr);
     }
 }
