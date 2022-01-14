99da58f7ac489451c7d0a447ad010790d8141961
Pedro Lima <contact.pedrolima@gmail.com>
2022-01-14 17:24:44 -0300

    Update poke_of_the_day.py

--- python_stuff/poke_of_the_day.py
+++ python_stuff/poke_of_the_day.py
@@ -6,6 +6,12 @@ html = requests.get(url=url).text
 soup = BeautifulSoup(html, 'html.parser')
 poke_day = soup.findAll('div', attrs={'class': 'dex tooltip2'})
 str_poke = str(poke_day)
-poke_of_the_day_name = str_poke[55:-309]
+lst = []
+for _ in str_poke[55:]:
+    if _ != '.':
+        lst.append(_)
+    else:
+        break
+poke_of_the_day_name = "".join(lst)
 
 print(poke_of_the_day_name)
