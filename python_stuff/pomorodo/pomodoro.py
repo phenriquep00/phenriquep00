cd45fd8b04ae022ea00829bbc480f1e0bd3022af
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-01 13:31:11 -0300

    created pomodoro timer class

--- python_stuff/pomorodo/pomodoro.py
+++ python_stuff/pomorodo/pomodoro.py
@@ -4,8 +4,9 @@ sg.theme('DarkGray5')
 
 layout = [
     [sg.Text('Pomodoro')],
-    [],
-    []
+    [sg.Button("Focus"), sg.Button("Short Break"), sg.Button("Long Break")],
+    [sg.Text("25"), sg.Text(":"), sg.Text("00")],
+    [sg.Button("Start")]
 ]
 
 window = sg.Window('Pomodoro', layout)
@@ -13,5 +14,6 @@ window = sg.Window('Pomodoro', layout)
 while True:
     event, values = window.read()
 
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
         break
+
