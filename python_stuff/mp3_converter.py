ca5fbab68cb233637a2e0411dda2faea3ec46d91
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-03 16:20:30 -0300

    repository refactoring

--- python_stuff/mp3_converter.py
+++ python_stuff/mp3_converter.py
@@ -11,11 +11,11 @@ def convert_to_mp3(link):
 
         name = str(ydl.prepare_filename(video))
 
-
     video = VideoFileClip(name)
     print(type(name))
     video.audio.write_audiofile(os.path.join('musics', f'{name[:-16]}.mp3'))  # Create a .mp3 file
     os.remove(name)  # Delete the .mp4 file
-    
+
+
 music = str(input('youtube link for the music: '))
 convert_to_mp3(music)
