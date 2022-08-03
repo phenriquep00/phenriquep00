0004fa46729baf96e83edc2a7691d74ab01d9d78
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-03 16:21:22 -0300

    words.py refactoring

--- python_stuff/words/main.py
+++ python_stuff/words/main.py
@@ -11,6 +11,7 @@ def get_word(word: str):
     except KeyError:
         return 'not a word'
 
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
             's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
